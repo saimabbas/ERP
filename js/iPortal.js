2a7a1b5a3a92b16c2ababbfc666e0d3b43d04b95
Saim Abbas <saimabbas@gmail.com>
2021-04-06 02:53:56 +0500

    first commit

---
+++ js/iPortal.js
@@ -0,0 +1,57 @@
+$(".card-blue-hover-effect").mouseenter(function(){
+    $(this).addClass("card-blue-hover animate wow fadeIn");
+});
+$(".card-blue-hover-effect").mouseleave(function(){
+    $(this).removeClass("card-blue-hover");
+});
+
+var swiper = new Swiper('.swiper-container', {
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: '3',
+    autoplay: 
+    {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    loop:true
+  });
+
+var swiper = new Swiper('.just-mob-swiper-testimony',{
+  autoplay: 
+    {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    // loop: true,
+    // centeredSlides: true,
+    // slidesPerView: '1',
+    // spaceBetween: 10
+});
+
+  
+
+  
+
+  $(".edit-popup").click(function(){
+  $("#pop-up-container").css({"display" : "flex"});
+  $("#pop-up-container").css({"backdrop-filter" : "blur(5px)"});
+  $("#pop-up-container").addClass("wow animated fadeIn");
+  $(".pop-up-edit").addClass("wow animated fadeInRight");
+});
+
+$(".popup-edit-close").click(function(){
+  $("#pop-up-container").css({"display" : "none"});
+});
+
+
+  
+
+
+
+
+
+
+
+
+  
